fb803bca52f4fb652322a455985cf3575f28f32a
Yehyus <yehyarefaat@hotmail.com>
2023-09-20 11:58:40 -0400

    Affichage des articles

--- single.php
+++ single.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 <main class="single">
-
+<?php
+    if (have_posts()): 
+        while (have_posts()) : the_post();
+            the_title('<h1>','</h1>');
+            the_content(); ?>
+            <hr>
+        <?php endwhile;
+    endif;
+?>
 
 </main>
 <?php get_footer(); ?>
