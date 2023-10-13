f697879b679f119fcc3d987d06ec0bb3242b7dd0
Audrey Dénommée <77136585+OreoBambolii@users.noreply.github.com>
2023-10-13 10:52:03 -0400

    Ajout de la flèche pour monter contenu des cours

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     <?php wp_head(); ?>
     <title>Document</title>
 </head>
