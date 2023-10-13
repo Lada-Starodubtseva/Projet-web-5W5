48a6fec559c2b50882f670c62cc4a2963544eeb9
Yehyus <yehyarefaat@hotmail.com>
2023-10-13 10:50:45 -0400

    Ajout cube décor

--- templates-parts/futur.php
+++ templates-parts/futur.php
@@ -1,6 +1,7 @@
 <h6>STAGE</h6>
 <h1>ATE</h1>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
     Aspernatur minus qui laborum eveniet ratione alias tempore quaerat distinctio, 
     incidunt ab porro debitis velit fugiat. Lorem ipsum dolor sit amet consectetur 
     adipisicing elit. Modi sed obcaecati, odit fugiat pariatur odio velit dolor 
@@ -10,11 +11,25 @@
     molestiae ut dicta excepturi odit. Ipsam earum animi omnis ducimus possimus nobis? 
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quisquam minima ab 
     beatae maxime facilis cum sunt, reiciendis autem quidem eius corrupti eos corporis 
-    esse numquam velit odio quo harum!</p>
+    esse numquam velit odio quo harum!
+</p>
 
-    <h6>STAGE</h6>
-    <h1>Fin d'études</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
+<div class="animation-cube">
+    <div class="cube1">
+        <div class="cube-deco1"></div>
+        <div class="cube-deco2"></div>
+    </div>
+
+    <div class="cube2">
+        <div class="cube-deco3"></div>
+        <div class="cube-deco4"></div>
+    </div>
+</div>
+
+<h6>STAGE</h6>
+<h1>Fin d'études</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
     Aspernatur minus qui laborum eveniet ratione alias tempore quaerat distinctio, 
     incidunt ab porro debitis velit fugiat. Lorem ipsum dolor sit amet consectetur 
     adipisicing elit. Modi sed obcaecati, odit fugiat pariatur odio velit dolor 
@@ -24,26 +39,31 @@
     molestiae ut dicta excepturi odit. Ipsam earum animi omnis ducimus possimus nobis? 
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quisquam minima ab 
     beatae maxime facilis cum sunt, reiciendis autem quidem eius corrupti eos corporis 
-    esse numquam velit odio quo harum!</p>
+    esse numquam velit odio quo harum!
+</p>
 
-    <h6>UNIVERSITÉ</h6>
-    <h1>Programmes</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
+<h6>UNIVERSITÉ</h6>
+<h1>Programmes</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
     Aspernatur minus qui laborum eveniet ratione alias tempore quaerat distinctio, 
-    incidunt ab porro debitis velit fugiat. </p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
+    incidunt ab porro debitis velit fugiat. 
+</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
 
-    <h6>MARCHÉ DU TRAVAIL</h6>
-    <h1>Entreprises</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
+<h6>MARCHÉ DU TRAVAIL</h6>
+<h1>Entreprises</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae sunt maxime nostrum esse. 
     Aspernatur minus qui laborum eveniet ratione alias tempore quaerat distinctio, 
-    incidunt ab porro debitis velit fugiat.</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
-    <p>-Lorem ipsum dolor, sit amet</p>
+    incidunt ab porro debitis velit fugiat.
+</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
+<p>-Lorem ipsum dolor, sit amet</p>
