0bcb1c01d7a6eda754f5ca1beb4e404c1a56a10b
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-10-30 13:41:49 -0400

    animations dans la section de projets

--- front-page.php
+++ front-page.php
@@ -57,25 +57,127 @@
 
                                 <div class="container">
 
-                                    <div class="cont-prof">
-                                        <div class="prof">
-                                            <h3 class="title">prof 1</h3>
+                                    <div class="cont-projet">
+                                        <div class="projet">
+                                            <h3 class="title">projet 1</h3>
                                         </div>
 
-                                        <div class="prof">
-                                            <h3 class="title">prof 2</h3>
+                                        <div class="projet">
+                                            <h3 class="title">projet 2</h3>
                                         </div>
 
-                                        <div class="prof">
-                                            <h3 class="title">prof 3</h3>
+                                        <div class="projet">
+                                            <h3 class="title">projet 3</h3>
                                         </div>
 
-                                        <div class="prof">
-                                            <h3 class="title">prof 4</h3>
+                                        <div class="projet">
+                                            <h3 class="title">projet 4</h3>
                                         </div>
 
-                                        <div class="prof">
-                                            <h3 class="title">prof 5</h3>
+                                        <div class="projet">
+                                            <h3 class="title">projet 5</h3>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <section class="carrousel-projets">
+            <title>Pojets des eleves</title>
+            <div id="background-deux"></div>
+            <div class="one">
+                <div class="two">
+                    <div class="three">
+                        <div class="four">
+                            <div class="five">
+
+
+                                <div class="container">
+
+                                    <div class="cont-projet">
+                                        <div id="projet1" class="projet" onclick="showText() , moveDiv()" >
+                                    
+                                            <h3 class="title">projet 1</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet2" class="projet" onclick="showText2() , moveDiv2()" >
+                                            <h3 class="title">projet 2</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText2" class="texteProjet2">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet3"  class="projet" onclick="showText3() , moveDiv3()" >
+                                            <h3 class="title">projet 3</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText3" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+
+                                        <div id="projet4" class="projet" onclick="showText4() , moveDiv4()" >
+                                            <h3 class="title">projet 4</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText4" class="texteProjet2">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
+                                        </div>
+                                        <div id="projet5" class="projet" onclick="showText5() , moveDiv5()" >
+                                            <h3 class="title">projet 5</h3>
+                                            <img src="" alt="">
+
+                                            <p id="projText5" class="texteProjet">
+                                                Lorem, ipsum dolor sit 
+                                                amet consectetur adipisicing 
+                                                elit. Nobis enim blanditiis 
+                                                est quam, porro, animi repellendus 
+                                                veniam libero, quas officia 
+                                                architecto similique! Ab voluptates 
+                                                laboriosam quibusdam tenetur aut dolor 
+                                                pariatur?
+                                            </p>
                                         </div>
                                     </div>
 
@@ -90,53 +192,18 @@
 
         </section>
 
-
-        <section class="projets">
-
-            <p class="titre">Projets
-            <br> etudiants</p>
-        
-       
-            <div class="gridProjets">
-                <div class="projet item1">
-                    
-                </div>
-                <div class="projet item2">
-
-                </div>
-                <div class="projet item3">
-
-                </div>
-                <div class="projet item4">
-
-                </div>
-                <div class="projet item5">
-
-                </div>
-                <div class="projet item6">
-                    
-                </div>
-                <div class="projet item7">
-
-                </div>
-                <div class="projet item8">
-
-                </div>
-                <div class="projet item9">
-
-                </div>
-                <div class="projet item10">
-
-                </div>
-                <div class="projet item11">
-
-                </div>
+ 
+  
+            
+            <div class="degrade">
+                <div class="premier"></div>
+                <div class="deuxieme"></div>
+                <div class="troisieme"></div>
+                <div class="quatrieme"></div>
+                <div class="cinquieme"></div>
             </div>
-             
-
         </section>
     </body>
-
 
 
 
@@ -144,3 +211,57 @@
 
 
 <?php get_footer(); ?>
+
+
+ 
+echo '<script type="text/JavaScript"> 
+
+    
+    function showText() {
+        var text = document.getElementById("projText");
+        text.classList.toggle("show");
+        }
+    function showText2() {
+        var text2 = document.getElementById("projText2");
+        text2.classList.toggle("show");
+        }
+    function showText3() {
+        var text3 = document.getElementById("projText3");
+        text3.classList.toggle("show");
+        }
+    function showText4() {
+        var text4 = document.getElementById("projText4");
+        text4.classList.toggle("show");
+        }
+    function showText5() {
+        var text5 = document.getElementById("projText5");
+        text5.classList.toggle("show");
+        }
+
+
+
+    function moveDiv() {
+        var proj = document.getElementById("projet1");
+        proj.style.left = proj.offsetLeft + -25 + "px";
+        }
+    function moveDiv2() {
+        var proj2 = document.getElementById("projet2");
+        proj2.style.left = proj2.offsetLeft + -170 + "px";
+    }
+    function moveDiv3() {
+        var proj3 = document.getElementById("projet3");
+        proj3.style.left = proj3.offsetLeft + -25 + "px";
+    }
+    function moveDiv4() {
+        var proj4 = document.getElementById("projet4");
+        proj4.style.left = proj4.offsetLeft + -170 + "px";
+    }
+    function moveDiv5() {
+        var proj5 = document.getElementById("projet5");
+        proj5.style.left = proj5.offsetLeft + -25 + "px";
+    }
+
+     </script>' 
+; 
+?> 
+
