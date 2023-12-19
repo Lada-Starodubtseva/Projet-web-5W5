24cf392f13204470df604ec97b84331ae18bd3aa
Yehyus <yehyarefaat@hotmail.com>
2023-12-18 20:22:05 -0500

    Ajout titre

--- templates-parts/futur.php
+++ templates-parts/futur.php
@@ -1,1 +1,4 @@
-<?php echo do_shortcode('[em_futur]'); ?>
+<section id="futur_titre" class="futur_titre">
+    <h2>CARRIÈRE PROFESSIONNELLE</h2>
+    <?php echo do_shortcode('[em_futur]'); ?>
+</section>
